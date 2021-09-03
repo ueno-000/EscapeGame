114e1e6b6bc5c55bdf17d1042b09ac2c91c49f98
uen0_ <sarasa4869@zoho.com>
2021-09-03 14:12:58 +0900

    １F2F3Fの追加

---
+++ Assets/Spricts/Button.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button: MonoBehaviour
+{
+    public GameObject button;
+
+    void Start()
+    {
+        button.SetActive(false);
+    }
+
+    //OnTriggerStay関数
+    //接触したオブジェクトが引数otherとして渡される
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        //接触しているオブジェクトのタグが"Player"のとき
+        if (other.CompareTag("Player"))
+        {
+            //オブジェクトの色を赤に変更する
+            GetComponent<Renderer>().material.color = Color.red;
+            button.SetActive(true);
+        }
+    }
+
+    //OnTriggerExit関数
+    //離れたオブジェクトが引数otherとして渡される
+    void OnTriggerExit2D(Collider2D other)
+    {
+        //離れたオブジェクトのタグが"Player"のとき
+        if (other.CompareTag("Player"))
+        {
+            //オブジェクトの色を赤に変更する
+            GetComponent<Renderer>().material.color = Color.yellow;
+            button.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Spricts/Sound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    /// <summary>マスカーソルが重なった時にならす音</summary>
+    [SerializeField] public AudioClip m_mouseEnter;
+    /// <summary>クリック音</summary>
+    [SerializeField] public AudioClip m_click;
+
+    private void Start()
+    {
+        if (!m_mouseEnter)
+        {
+            Debug.LogError("効果音をアサインしてください");
+        }
+        if (!m_click)
+        {
+            Debug.LogError("効果音をアサインしてください");
+        }
+    }
+}
