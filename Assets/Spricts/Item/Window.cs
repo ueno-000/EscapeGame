ad335369a9df2fa3925445f1da12e820c6037b15
uen0_ <sarasa4869@zoho.com>
2021-09-14 19:32:59 +0900

    Window,Potの画像ラフの追加

--- Assets/Spricts/Item/Window.cs
+++ Assets/Spricts/Item/Window.cs
@@ -9,7 +9,7 @@ public class Window : ItemBase
         if (!m_isChecked)
         {
             Debug.Log("窓を調べてみた");
-            //TextController.Instance.DisplayText("ピースを見つけた");
+            //TextController.Instance.DisplayText("なにか見つけた");
             m_isChecked = true;
             m_selectButton.SetActive(false);
             //if (m_isMustItem)
