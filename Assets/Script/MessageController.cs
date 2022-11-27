e32037188d809fcbe86d57138b54ff0a74916673
ueno-000 <sarasa4869@zoho.com>
2022-11-27 23:51:00 +0900

    [Add] メッセージを表示するscriptの追加

---
+++ Assets/Script/MessageController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageController : MonoBehaviour
+{
+    [SerializeField] private Text _messageTextBox;
+    [SerializeField] private string[] _massages;
+    [SerializeField] private float _textDuration;
+
+    private int i;
+    private float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _messageTextBox = _messageTextBox.gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_massages.Length <= i){ return;}
+
+        time += Time.deltaTime;
+
+        if (time >= _textDuration)
+        {
+            _messageTextBox.text = _massages[i].ToString();
+            time = 0f;
+            i++;
+        }
+    }
+}
