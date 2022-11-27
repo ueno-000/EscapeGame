c502742fdbab5b51c42abe210224ed89d4475fd9
ueno-000 <sarasa4869@zoho.com>
2022-11-27 23:51:33 +0900

    [Add] メッセージの処理を行うscript

---
+++ Assets/Script/MessageSequencer.cs
@@ -0,0 +1,74 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class MessagePrinter : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text _textUi = default;
+
+    [SerializeField]
+    private string _message = "";
+
+    [SerializeField]
+    private float _speed = 1.0F;
+
+    private float _elapsed = 0; // 文字を表示してからの経過時間
+    private float _interval; // 文字毎の待ち時間
+
+    // _message フィールドから表示する現在の文字インデックス。
+    // 何も指していない場合は -1 とする。
+    private int _currentIndex = -1;
+
+    public bool IsPrinting
+    {
+        get
+        {
+            // TODO: ここにコードを書く
+            if (_message is null or { Length: 0 }) return false;
+            return _currentIndex + 1 < _message.Length;
+        }
+    }
+
+    private void Start()
+    {
+        ShowMessage(_message);
+    }
+
+    private void Update()
+    {
+        if (_textUi is null || _message is null || _currentIndex + 1 >= _message.Length) { return; }
+
+        _elapsed += Time.deltaTime;
+        if (_elapsed > _interval)
+        {
+            _elapsed = 0;
+            _currentIndex++;
+            _textUi.text += _message[_currentIndex];
+        }
+    }
+
+    /// <summary>
+    /// 指定のメッセージを表示する。
+    /// </summary>
+    /// <param name="message">テキストとして表示するメッセージ。</param>
+    public void ShowMessage(string message)
+    {
+        // TODO: ここにコードを書く
+        _textUi.text = "";
+        _currentIndex = -1;
+        _message = message;
+        _interval = _message is null or { Length: 0 } ? 0 : _speed / _message.Length;
+    }
+
+    /// <summary>
+    /// 現在再生中の文字出力を省略する。
+    /// </summary>
+    public void Skip()
+    {
+        // TODO: ここにコードを書く
+        if (_message is null) { return; }
+
+        _currentIndex = _message.Length;
+        _textUi.text = _message;
+    }
+}
